6bf41d6aebec1d1c26c6eb7ec79aae0ac718206f
Lim Tian Hee <thlim2009@hotmail.com>
2023-01-02 18:53:19 +0800

    feat: website header

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/global.css" />
+</head>
+
+<body>
+    <?php include "components/header.php" ?>
+    <div id="home-container"></div>
+</body>
+
+</html>

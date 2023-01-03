aed0a7fa7ad245a24ebd0ed04bdf048c19872b26
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2023-01-03 22:45:24 +0800

    requested modify done

--- edit-profile.php
+++ edit-profile.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Edit Profile</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -14,7 +14,7 @@
             <h1>Profile</h1>
             <img src="images/c++.png" class="rounded-profile-image" style="width:150px; object-fit:cover">
         </div>
-        <form action="" method="post">
+        <form action="profile_edit.php" method="post" enctype="multipart/form-data">
             <div class="input-box">
                 <label for="username" style="font-size: 1.2em;">User Name</label>
                 <!-- placeholder will be the user's infomation -->
@@ -24,6 +24,16 @@
                 <label for="email" style="font-size: 1.2em;">Email Address</label>
                 <!-- placeholder will be the user's infomation -->
                 <input type="email" name="email" placeholder="1191302145@student.mmu.edu">
+            </div>
+            <div class="input-box">
+                <label for="password1" style="font-size: 1.2em;">Password</label>
+                <!-- placeholder will be the user's infomation -->
+                <input type="password" name="password1" placeholder="password">
+            </div>
+            <div class="input-box">
+                <label for="password2" style="font-size: 1.2em;">Confirm Password</label>
+                <!-- placeholder will be the user's infomation -->
+                <input type="password" name="password2" placeholder="password">
             </div>
             <div class="input-box">
                 <label for="photo" style="font-size: 1.2em;">Profile Picture</label>

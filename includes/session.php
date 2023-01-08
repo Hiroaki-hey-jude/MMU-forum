6a0e759d8eb193ac4606096cf619ee22b2d1eaee
XingSc29 <neetxing@gmail.com>
2023-01-08 18:00:27 +0800

    Fixed session.php

--- includes/session.php
+++ includes/session.php
@@ -3,17 +3,17 @@
 	session_start();
 	
 	// admin session for future
-	if(isset($_SESSION['admin'])){
-		header('location: admin/home.php');
-	}
+	//if(isset($_SESSION['admin'])){
+	//	header('location: admin/home.php');
+	//}
 
-	if(isset($_SESSION['user'])){
-		$conn = $pdo->open();
-
+	if(isset($_SESSION['user'])) {
 		try{
-			$stmt = $conn->prepare("SELECT * FROM users WHERE id=:id");
-			$stmt->execute(['id'=>$_SESSION['user']]);
-			$user = $stmt->fetch();
+			$stmt = $conn->prepare("SELECT * FROM users WHERE id= ?");
+			$stmt->bind_param("i", $_SESSION['user']);
+			$stmt->execute();
+
+			$user = $stmt->get_result()->fetch_assoc();
 		}
 		catch(PDOException $e){
 			echo "There is some problem in connection: " . $e->getMessage();

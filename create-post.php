4e031c8224858e68f0fc0cbad74766107cf52195
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2023-01-12 10:49:07 +0800

    javascript and php still nost done but UI done

---
+++ create-post.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/createpost.css">
+    <link rel="stylesheet" href="css/header.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body style="background-color: #dae0e6;">
+<div class="header-div"></div>
+    <header class="primary-background">
+        <a id="header-title" href="home.php">
+            <img id="header-image" src="images/MMU.png" height="30px" />
+            <span class="header-span"></span>
+            MMU Forum
+        </a>
+        <span class="header-span"></span>
+        <span class="header-span"></span>
+        <div class="search-bar">
+            <input class="search-input" type="text" name="search" placeholder="Search for post" />
+            <!-- TODO: add search feature -->
+            <a href="#search"><span class="fas fa-search search-icon"></span></a>
+        </div>
+        <span style="flex: 1;"></span>
+        <a id="header-user" href="#user"><span class="fas fa-user user-icon"></span></a>
+    </header>
+    <div class="edit-post-container">
+        <div class="top-h2">
+            <h2>Create a Post</h2>
+        </div>
+        <form action="" method="post">
+            <div class="choose-category">
+                <div class="category">
+                    <select name="category-names" id="category-names" style="width:200px;">
+                        <option value="" disabled selected>Select your Category</option>
+                        <option value="rigatoni">Rigatoni</option>
+                        <option value="dave">Dave</option>
+                        <option value="pumpernickel">Pumpernickel</option>
+                        <option value="reeses">Reeses</option>
+                    </select>
+                </div>
+                <div class="subcategory">
+                    <select name="subcategory-names" id="subcategory-names" style="width:200px;">
+                        <option value="" disabled selected>Select your Subcategory</option>
+                        <option value="rigatoni">Rigatoni</option>
+                        <option value="dave">Dave</option>
+                        <option value="pumpernickel">Pumpernickel</option>
+                        <option value="reeses">Reeses</option>
+                    </select>
+                </div>
+            </div>
+            <!-- by using contenteditable="true", it it gonna be like textarea but but put images also  -->
+            <p>*Place sentences and images to post!</p>
+            <div class="post-create" id="alt_textarea" contenteditable="true"></div>
+            <div align="right" class="post-submit-button">
+                <input type="submit" id="submit_btn" name="submit">
+            </div>
+        </form>
+    </div>
+</body>
+</html>

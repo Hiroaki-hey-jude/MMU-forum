69713f1e49e9c06f9ddcb40e8c3cbb432c1d8d6a
Lim Tian Hee <thlim2009@hotmail.com>
2023-01-13 21:23:15 +0800

    feat: setup post list title

--- post-list.php
+++ post-list.php
@@ -3,6 +3,32 @@
     <?php
         $posts[] = array("p0000", "Announcement", 3, 10, "Lim", "12/12/2022", true);
         $posts[] = array("p0001", "Online Week", 3, 10, "Lim", "12/12/2022", false);
+
+        $search = $_GET["search"];
+        // passed from where the list was accessed
+        $type = $_GET["type"];
+        // TODO: (either one)
+        // 1. based on the selected list type and id, query the name of the selected title
+        // - id is already expected to be passed so no need to specifically pass title, but more steps on current page
+        $parentTitle = "FCI";
+        // 2. pass title directly from previous page
+        // less steps, but everytime you need to pass title specifically to this page
+        // $parentTitle = $_GET["title"];
+
+        switch ($type) {
+            case 'category':
+                $title = "Subcategories in ".$parentTitle;
+                break;
+            case 'subcategory':
+                $title = "Posts in ".$parentTitle;
+                break;
+            default:
+                $title = "Posts";
+                break;
+        }
+        if ($search) {
+            $title = "Results of ".$search;
+        }
     ?>
     <head>
         <link rel="stylesheet" href="css/reset.css" />
@@ -15,7 +41,7 @@
         <div id="page-container">
             <div class="post-list">
                 <div class="list-title primary-background">
-                    Result
+                    <?php echo $title ?>
                 </div>
                 <?php
                     foreach($posts as $item){

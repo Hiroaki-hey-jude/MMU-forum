90faf6429dff4f7268d4d12c33d02ad8657d41c1
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2023-01-16 12:29:51 +0800

    done create post page

--- create-post.php
+++ create-post.php
@@ -1,14 +1,81 @@
 <?php
 //backend data
+$categories[] = array(
+    //`category_id`, `category_name`
+    "1",
+    "GENERAL", 
+);
+$categories[] = array(
+    "2",
+    "aculty of Computing and Informatics (FCI)", 
+);
+$categories[] = array(
+    "3",
+    "Faculty of Engineering (FOE) & Faculty of Engineering & Technology (FET)", 
+);
+$categories[] = array(
+    "4",
+    "Faculty of Management (FOM) & Faculty of Business (FOB)", 
+);
+$categories[] = array(
+    "5",
+    "Faculty of Creative Multimedia (FCM)", 
+);
+$categories[] = array(
+    "6",
+    "Faculty of Applied Communication (FAC)", 
+);
+$categories[] = array(
+    "7",
+    "Fad (FED)", 
+);
 
-
+// $subcategories[] = array(
+//     "1", "Annoucement", "1", "18", "46"
+// );
+$subcategories[] = array(
+    //`subcategory_id`, `subcategory_name`, `category_id`, `number_of_posts`, `number_of_comments`
+    "1", "Annoucement", "1", "18", "46"
+);
+$subcategories[] = array(
+    "2", "Club Activities", "1", "16", "46"
+);
+$subcategories[] = array(
+    "3", "Feedback and Helpdesk", "1", "16", "46"
+);
+$subcategories[] = array(
+    "4", "Admission & Enrollment", "1", "22", "72"
+);
+$subcategories[] = array(
+    "5", "Hostel Discussion", "1", "15", "44"
+);
+$subcategories[] = array(
+    "6", "Computer Science in General", "2", "15", "44"
+);
+$subcategories[] = array(
+    "7", "Nanotechnology", "3", "15", "44"
+);
+$subcategories[] = array(
+    "8", "Digital Enterprise", "4", "15", "44"
+);
+$subcategories[] = array(
+    "9", "Virtual Reality", "5", "15", "44"
+);
+$subcategories[] = array(
+    "10", "Media Culture", "6", "15", "44"
+);
+$subcategories[] = array(
+    "11", "Movie Production", "7", "15", "44"
+);
+$subcategories_clone = $subcategories;
+$subcategories =  json_encode($subcategories);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/createpost.css">
     <link rel="stylesheet" href="css/header.css" />
@@ -36,35 +103,60 @@
         <div class="top-h2">
             <h2>Create a Post</h2>
         </div>
-        <form action="upload/path" method="post">
+        <form action="" method="post">
             <div class="choose-category">
                 <div class="category">
-                    <select name="category-names" id="category-names" style="width:200px;">
-                        <option value="" disabled selected>Select your Category</option>
-                        <option value="rigatoni">Rigatoni</option>
-                        <option value="dave">Dave</option>
-                        <option value="pumpernickel">Pumpernickel</option>
-                        <option value="reeses">Reeses</option>
+                    <select name="category-names" id="category-names" style="width:200px;" onchange="changeSubCategory()">
+                    <option value="" disabled selected>Select your Category</option>
+                    <?php
+                    foreach($categories as $category){
+                        echo '
+                        <option id="'.$category[0].'" value="'.$category[0].'">'.$category[1].'</option>
+                            ';
+                    }
+                    ?>
                     </select>
                 </div>
                 <div class="subcategory">
                     <select name="subcategory-names" id="subcategory-names" style="width:200px;">
                         <option value="" disabled selected>Select your Subcategory</option>
-                        <option value="rigatoni">Rigatoni</option>
-                        <option value="dave">Dave</option>
-                        <option value="pumpernickel">Pumpernickel</option>
-                        <option value="reeses">Reeses</option>
+                        <?php
+                        foreach($subcategories_clone as $subcategory){
+                            echo '
+                            <option id="'.$subcategory[1].'" value="'.$subcategory[1].'">'.$subcategory[1].'</option>
+                            ';
+                        }    
+                        ?>
                     </select>
                 </div>
             </div>
-            <!-- by using contenteditable="true", it it gonna be like textarea but but put images also  -->
-            <p style="font-size: 1.1rem;">*<span style="color:red; font-weight:bold;">WRITE</span> sentences and <span style="color:red; font-weight:bold;">DRAG&DROP</span> images to the box!</p>
-            <div class="post-create" id="create_post_textarea" contenteditable="true"></div>
+            <input class="title-input" type="text" name="title" required placeholder="Enter Title">
+            <input class="image-input" type="text" name="image" required placeholder="Put image link">
+            <textarea name="description" id="" cols="30" rows="20" placeholder="Enter Description"></textarea>
             <div align="right" class="post-submit-button" id="text-area">
                 <input type="submit" id="submit_btn" name="submit">
             </div>
         </form>
     </div>
 </body>
-<script src="components/create-post.js"></script>
+<script>
+    let subcategoriesFromPHP = JSON.parse('<?php echo $subcategories; ?>');
+    //console.log('typeof: ' + typeof subcategoriesFromPHP);
+    for (let i = 0; i < subcategoriesFromPHP.length; i++) {
+        console.log('value: ' + subcategoriesFromPHP[i]);
+    }
+    function changeSubCategory() {
+        let selectedCategoryIndex = document.getElementById('category-names').value;
+
+        console.log(selectedCategoryIndex);
+        for(let i = 0; i < subcategoriesFromPHP.length; i++) {
+            if(selectedCategoryIndex != subcategoriesFromPHP[i][2]) {
+                console.log(subcategoriesFromPHP[i][1]);
+                document.getElementById(subcategoriesFromPHP[i][1]).style.display = "none";
+            } else {
+                document.getElementById(subcategoriesFromPHP[i][1]).style.display = "block";
+            }
+        }
+    }
+</script>
 </html>

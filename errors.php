0cf1147ce57983fd7c0add6d39d863ee2add3227
XingSc29 <neetxing@gmail.com>
2023-01-17 23:51:04 +0800

    Display errors in an alert box instead of displaying in the webpage

--- errors.php
+++ errors.php
@@ -1,7 +1,7 @@
 <?php if (count($errors) > 0): ?>
 	<div class="error">
 		<?php foreach ($errors as $error) : ?>
-			<p><?php echo $error ?></p>
+			<script>alert('<?php echo $error ?>');</script>
 		<?php endforeach ?>
 	</div>
 <?php	endif ?>

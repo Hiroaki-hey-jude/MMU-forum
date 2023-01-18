187b35c0f5a0af9f346f401a0b79b524a23da5e9
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2023-01-18 21:38:44 +0800

    added header to create-post.php and edit-profile.php

--- create-post.php
+++ create-post.php
@@ -79,23 +79,7 @@ include 'errors.php';
     <script src="https://kit.fontawesome.com/f019d50a29.js" crossorigin="anonymous"></script>
 </head>
 <body style="background-color: #dae0e6;">
-<div class="header-div"></div>
-    <header class="primary-background">
-        <a id="header-title" href="home.php">
-            <img id="header-image" src="images/MMU.png" height="30px" />
-            <span class="header-span"></span>
-            MMU Forum
-        </a>
-        <span class="header-span"></span>
-        <span class="header-span"></span>
-        <div class="search-bar">
-            <input class="search-input" type="text" name="search" placeholder="Search for post" />
-            <!-- TODO: add search feature -->
-            <a href="#search"><span class="fas fa-search search-icon"></span></a>
-        </div>
-        <span style="flex: 1;"></span>
-        <a id="header-user" href="#user"><span class="fas fa-user user-icon"></span></a>
-    </header>
+<?php include "components/header.php" ?>
     <div class="edit-post-container">
         <div class="top-h2">
             <h2>Create a Post</h2>

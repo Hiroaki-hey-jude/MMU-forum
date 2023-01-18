54325feda02f67850510877b93c461cd15d597df
XingSc29 <neetxing@gmail.com>
2023-01-18 22:29:53 +0800

    completed delete-post.php

--- delete-post.php
+++ delete-post.php
@@ -1,20 +1,25 @@
 <?php
 include "includes/session.php";
+include "includes/permission.php";
+include "includes/conn.php";
 $errors = array();
 if (isset($admin)) {
     if(isset($_GET['id'])) {
         $post_id = $_GET['id'];
         $query_delete_post = $conn->prepare("DELETE FROM post WHERE post_id = ?");
-        $query_delete_post->bind_param("s", $post_id);
+        $query_delete_post->bind_param("i", $post_id);
         if ($query_delete_post->execute()) {
-            // echo "<script>alert('Delete post ".$post_id." sucessfully');</script>)";
+            echo "<script>alert('Delete post ".$post_id." sucessfully');</script>";
             header("Location: {$_SERVER['HTTP_REFERER']}");
         } else {
-            // echo "<script>alert('Cannot execute delete post query');</script>";
+            echo "<script>alert('Cannot execute delete post query');</script>";
         }
     }
-} else {
-    array_push($errors, "You have no priviledge");
-    // header("Location: home.php");
+} 
+else {
+    array_push($errors, "You have no privilege");
 }
+include 'errors.php';
+
 ?>
+

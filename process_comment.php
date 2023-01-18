0e17bc62400452726194c2299b65869f5296f3c7
xavier <xavier9276@gmail.com>
2023-01-18 16:02:07 +0800

    fixed: changed add comment query to prepare statement

--- process_comment.php
+++ process_comment.php
@@ -6,9 +6,9 @@ if(isset($_POST['post_id']) && isset($_POST['user_id']) && isset($_POST['comment
     $user_id = $_POST['user_id'];
     $post_id = $_POST['post_id'];
     $comment_description = $_POST['comment'];
-    $query_insert_comment = "INSERT INTO comment (commenter_id, post_id, comment_description) VALUES (
-        ".$user_id.",".$post_id.", '".$comment_description."');";
-    if(mysqli_query($conn, $query_insert_comment)){
+    $query_insert_comment = $conn->prepare("INSERT INTO comment (commenter_id, post_id, comment_description) VALUES (?,?,?);");
+    $query_insert_comment->bind_param("iis", $user_id, $post_id, $comment_description);
+    if($query_insert_comment->execute()){
         echo 'success';
     } else {
         echo 'error';
